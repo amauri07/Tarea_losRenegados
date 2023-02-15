c384ad6962667b790692c149ff1565331642b23e
ejose <ejose@HP-Amauri>
2023-02-14 19:47:23 -0600

    ejercicios

---
+++ src/unidad2/Ejercicio2_31.java
@@ -0,0 +1,22 @@
+package unidad2;
+
+public class Ejercicio2_31 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("\n numero cuadrado cubo");
+		System.out.printf("\n %6d, %6d, %6d",0,0*0,0*0*0);
+		System.out.printf("\n %6d, %6d, %6d",1,1*1,1*1*1);
+		System.out.printf("\n %6d, %6d, %6d",2,2*2,2*2*2);
+		System.out.printf("\n %6d, %6d, %6d",3,3*3,3*3*3);
+		System.out.printf("\n %6d, %6d, %6d",4,4*4,4*4*4);
+		System.out.printf("\n %6d, %6d, %6d",5,5*5,5*5*5);
+		System.out.printf("\n %6d, %6d, %6d",6,6*6,6*6*6);
+		System.out.printf("\n %6d, %6d, %6d",7,7*7,7*7*7);
+		System.out.printf("\n %6d, %6d, %6d",8,8*8,8*8*8);
+		System.out.printf("\n %6d, %6d, %6d",9,9*9,9*9*9);
+		System.out.printf("\n %6d, %6d, %6d",10,10*10,10*10*10);
+
+	}
+
+}
---
+++ src/unidad2/Ejercicio2_33.java
@@ -0,0 +1,39 @@
+package unidad2;
+
+import java.util.Scanner;
+
+public class Ejercicio2_33 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try (Scanner input = new Scanner(System.in)){
+			System.out.print("peso en Kilogramo: ");
+			double peso = input.nextDouble();
+			System.out.print("altura en metros: ");
+			double altura = input.nextDouble();
+			double bmi = peso/(altura*altura);
+			System.out.println("su masa corporal es: "+ bmi);
+			
+			if (bmi > 30) {
+				System.out.println("obesidad");
+			}
+			else if (bmi < 30 &&bmi > 25){
+				System.out.println("sobre peso");
+			}
+			else if (bmi < 25 &&bmi > 18.5){
+				System.out.println("peso normal");
+			}
+			else if (bmi <= 18.5) {
+				System.out.println("bajo peso");
+			}
+		}
+		System.out.println(" ");
+		System.out.println("*****BMI valores*****");
+		System.out.println("bajo peso: menos de 18.5");
+		System.out.println("normal: entre 18.5 y 24.9");
+		System.out.println("sobrepeso: entre 25 y 29.9");
+		System.out.println("obeso: 30 o mas");
+
+	}
+
+}
